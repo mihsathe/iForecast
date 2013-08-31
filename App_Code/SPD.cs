8291c7cd6f810e48063807a6583ef906dc8c6ce0
Mihir Sathe <msathe@usc.edu>
2013-08-30 23:39:38 -0700

    New commit

---
+++ App_Code/SPD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class SPD
+{
+    List<Match> matches;
+
+	public SPD(List<Match> matches)
+	{
+        this.matches = matches;
+	}
+
+    public void calculate()
+    {
+        
+    }
+}
